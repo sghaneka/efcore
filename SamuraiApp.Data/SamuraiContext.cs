d0e8f94e2ca4f445e077bdfc187e6e2c079ea7ab
Sam Ghanekar <sghanekar@creditsights.com>
2017-05-11 20:18:48 -0400

    first

---
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+
+        public DbSet<Battle> Battles { get; set; }
+
+        public DbSet<Quote> Quotes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SamuraiBattle>()
+                .HasKey(s => new { s.BattleId, s.SamuraiId });
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(local)\\CSNEXT;Initial Catalog=PieShop;Integrated Security=SSPI;");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+
+    }
+}
---
+++ SomeUI/Program.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Logging;
+using SamuraiApp.Data;
+using SamuraiApp.Domain;
+using System;
+using System.Linq;
+
+namespace SomeUI
+{
+    class Program
+    {
+        private static SamuraiContext _context = new SamuraiContext();
+        static void Main(string[] args)
+        {
+            _context.GetService<ILoggerFactory>().AddProvider(new MyLoggerProvider());
+            // InsertSamurai();
+            // InsertMultipleSamurais();
+            MoreQueries();
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+
+        static void MoreQueries()
+        {
+            var name = "Sampson";
+            var samurais = _context.Samurais.Where(s => s.Name == name).ToList();
+            var x = _context.Samurais.FirstOrDefault(s => s.Name == "Julie");
+        }
+
+        static void SimpleSamuraiQuery()
+        {
+            using (var context = new SamuraiContext())
+            {
+                context.GetService<ILoggerFactory>().AddProvider(new MyLoggerProvider());
+                var samurais = context.Samurais.ToList();
+            }
+        }
+
+        static void InsertMultipleSamurais()
+        {
+            var samurai = new Samurai { Name = "Julie" };
+            var samuraiSammy = new Samurai() { Name = "Sampson" };
+            using (var context = new SamuraiContext())
+            {
+                context.GetService<ILoggerFactory>().AddProvider(new MyLoggerProvider());
+                context.Samurais.Add(samurai);
+                context.Samurais.Add(samuraiSammy);
+                context.SaveChanges();
+                //context.Add(samurai);
+            }
+        }
+
+        static void InsertSamurai()
+        {
+            var samurai = new Samurai { Name = "Julie" };
+            using (var context = new SamuraiContext())
+            {
+                context.GetService<ILoggerFactory>().AddProvider(new MyLoggerProvider());
+                context.Samurais.Add(samurai);
+                context.SaveChanges();
+                //context.Add(samurai);
+            }
+        }
+    }
+}
